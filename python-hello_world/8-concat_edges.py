60456c38538bf4ab4238bd387f9dccd2706d58f0
root <root@ph4nt0m.localdomain>
2026-06-19 23:59:28 +0200

    repos

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton"
-print(word[3:] + word[:3])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112])
